ab3cf2721e83bad164e0ac5536eef80607fa8f52
Seol Cheon <jojo432143@gmail.com>
2020-07-09 01:43:22 -0400

    yarn installed

--- COMP125-M2020-MidTerm/Scripts/app.js
+++ COMP125-M2020-MidTerm/Scripts/app.js
@@ -1,9 +1,16 @@
 // setup your IIFE (Immediately Invoked Function Expression)
+/*File name : app.js
+  Author's name : Seol Cheon
+  Web site name : COMP125-Midterm Test
+  file description: a text file containin JavaScript code that is used to execute */
 (function () {
 "use strict";
 
 
+
 // YOUR CODE GOES HERE
+
+
 
 
 })();
